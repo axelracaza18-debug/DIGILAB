7cffe3a35fa9b8ce3500d767e000fc54cd15b18f
Axllooot <axelracaza18@gmail.com>
2026-06-26 22:47:05 +0800

    Refactor statistics into StatisticsManager

--- src/Main.java
+++ src/Main.java
@@ -171,40 +171,9 @@ public class Main {
 
                     break;
 
-                case 6:
+                    case 6:
 
-                    if (patients.isEmpty()) {
-
-                        System.out.println("No patients found.");
-
-                    } else {
-
-                        System.out.println("\n=== PATIENT STATISTICS ===");
-                        System.out.println("Total Patients: " + patients.size());
-                        int totalAge = 0;
-                        Patient oldestPatient = patients.get(0);
-                        Patient youngestPatient = patients.get(0);
-                        for (Patient p : patients) {
-
-                            totalAge += p.age;
-
-                            if (p.age > oldestPatient.age) {
-
-                                oldestPatient = p;
-
-                            }
-                            if (p.age < youngestPatient.age) {
-
-                                youngestPatient = p;
-
-                            }
-                        }
-                        double averageAge = (double) totalAge / patients.size();
-
-                        System.out.println("Average Age: " + averageAge);
-                        System.out.println("Oldest Patient: " + oldestPatient.name + " (" + oldestPatient.age + ")");
-                        System.out.println("Youngest Patient: " + youngestPatient.name + " (" + youngestPatient.age + ")");
-                    }
+                    StatisticsManager.showStatistics(patients);
 
                     break;
 
